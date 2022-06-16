9fd74fbf1a48a63611f8299dff4611cb9324b65f
Erick Ribeiro <erickrocha965@gmail.com>
2022-06-16 20:14:44 -0300

    feat: adicionar dados aos gráficos do darshboard

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Pátio", "Banheiro", "Refeitório", "Sala de aula",  "Outro"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [40, 10, 20, 25, 5],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc', '#e74a3b', '#f6c23e'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf', '#c54034', '#c59b32!'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
