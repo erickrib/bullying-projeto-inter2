634d1f749d495f23f1cb442f227e6913ad9ad63d
Erick Ribeiro <erickrocha965@gmail.com>
2022-06-16 17:04:22 -0300

    feat: opção de editar mensagem enviada

--- vendor/main.js
+++ vendor/main.js
@@ -4,7 +4,7 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-(function() {
+(function () {
   "use strict";
 
   /**
@@ -75,6 +75,22 @@
     })
   }
 
+  const buttonEdit = document.getElementsByClassName('edit-button')[0]
+
+  buttonEdit.addEventListener('click', () => {
+    let textarea = document.getElementsByClassName('text-area')[0]
+    
+    if (buttonEdit.innerHTML === 'Editar') {
+      textarea.removeAttribute("readonly")
+      buttonEdit.innerHTML = 'Salvar'
+      buttonEdit.classList.add("bg-success")
+    } else {
+      textarea.setAttribute("readonly", true)
+      buttonEdit.innerHTML = 'Editar'
+      buttonEdit.classList.remove("bg-success")
+    }
+  })
+
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
@@ -110,7 +126,7 @@
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
+  on('click', '.mobile-nav-toggle', function (e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
@@ -119,7 +135,7 @@
   /**
    * Mobile nav dropdowns activate
    */
-  on('click', '.navbar .dropdown > a', function(e) {
+  on('click', '.navbar .dropdown > a', function (e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
       this.nextElementSibling.classList.toggle('dropdown-active')
@@ -129,7 +145,7 @@
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function(e) {
+  on('click', '.scrollto', function (e) {
     if (select(this.hash)) {
       e.preventDefault()
 
@@ -204,9 +220,9 @@
 
       let portfolioFilters = select('#portfolio-flters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#portfolio-flters li', function (e) {
         e.preventDefault();
-        portfolioFilters.forEach(function(el) {
+        portfolioFilters.forEach(function (el) {
           el.classList.remove('filter-active');
         });
         this.classList.add('filter-active');
